f2ee717a9441e9052ea40a4baf6e893ece61c70a
Alex.T <alex.rabota.555@gmail.com>
2021-06-10 21:46:18 +0300

    Fixed init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ sudo /etc/init.d/nginx restart
 
 # sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.conf.py
 sudo /etc/init.d/gunicorn stop
-gunicorn -c etc/guniconr.conf.py hello:app
+gunicorn -c etc/gunicorn.conf.py hello:app
 
 # sudo /etc/init.d/mysql start
