2bdb5eba4ac24d4d94b557bed5f920610ea41be8
Alex.T <alex.rabota.555@gmail.com>
2021-06-10 21:40:02 +0300

    Changed init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-# sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
+
+# sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.conf.py
+sudo /etc/init.d/gunicorn stop
+gunicorn -c etc/guniconr.conf.py hello:app
+
 # sudo /etc/init.d/mysql start
