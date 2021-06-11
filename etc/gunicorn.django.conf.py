4f3c28ec2a359a4111181b13f319098c832f8eb9
Alex.T <alex.rabota.555@gmail.com>
2021-06-12 02:24:50 +0300

    Fixed etc/gunicorn.django.conf.py

--- etc/gunicorn.django.conf.py
+++ etc/gunicorn.django.conf.py
@@ -3,9 +3,9 @@ gunicorn Django config file
 
 """
 import multiprocessing
-# from pathlib import Path
+from pathlib import Path
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 
-# pythonpath = Path(__file__).absolute().parent.parent / 'ask'
+pythonpath = str(Path(__file__).absolute().parent.parent / 'ask')
