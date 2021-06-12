7530a21cf273eecc504ae8387ea1e76e89ca81f5
Alex.T <alex.rabota.555@gmail.com>
2021-06-12 04:03:18 +0300

    Changed ask/ask/urls.py and ask/qa/urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,16 +1,24 @@
 """
     ask/qa/urls.py
 """
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
-    path('login/', views.test, name='test'),
-    path('signup/', views.test, name='test'),
-    path('question/', views.test, name='test'),
-    path('ask/', views.test, name='test'),
-    path('popular/', views.test, name='test'),
-    path('new/', views.test, name='test'),
+    url(r'^$', views.test, name='test'),
+    url(r'^login/', views.test, name='test'),
+    url(r'^signup/', views.test, name='test'),
+    url(r'^question/', views.test, name='test'),
+    url(r'^ask/', views.test, name='test'),
+    url(r'^popular/', views.test, name='test'),
+    url(r'^new/', views.test, name='test'),
+    # path('', views.test, name='test'),
+    # path('login/', views.test, name='test'),
+    # path('signup/', views.test, name='test'),
+    # path('question/', views.test, name='test'),
+    # path('ask/', views.test, name='test'),
+    # path('popular/', views.test, name='test'),
+    # path('new/', views.test, name='test'),
 ]
