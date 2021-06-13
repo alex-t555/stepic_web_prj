ac386a62c0ccca970cfdb29c59a1ff7316dd1739
Alex.T <alex.rabota.555@gmail.com>
2021-06-13 22:44:46 +0300

    Fixed init.sh, init_db.sh

--- init.sh
+++ init.sh
@@ -5,9 +5,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 # sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.conf.py
-sudo ln -sf /home/box/web/etc/gunicorn.django.conf.py   /etc/gunicorn.d/test.django.conf.py
+# sudo ln -sf /home/box/web/etc/gunicorn.django.conf.py   /etc/gunicorn.d/test.django.conf.py
 sudo /etc/init.d/gunicorn stop
-# gunicorn -c /etc/gunicorn.d/test.conf.py hello:app
-gunicorn -c /etc/gunicorn.d/test.django.conf.py ask.wsgi:application
-
-sudo /etc/init.d/mysql start
+# gunicorn -c /etc/gunicorn.d/test.conf.py hello:app&
+# gunicorn -c /etc/gunicorn.d/test.django.conf.py ask.wsgi:application&
+gunicorn -c etc/gunicorn.django.conf.py ask.wsgi:application&
--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
+sudo /etc/init.d/mysql start
+
 mysql -uroot -e "create database ask;"
 mysql -uroot -e "grant all privileges on ask.* to 'box'@'localhost' with grant option;"
+
 ~/web/ask/manage.py makemigrations
 ~/web/ask/manage.py migrate
