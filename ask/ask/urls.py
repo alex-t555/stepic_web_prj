ff4c133c118462dffdc6b61e82f25ec5bd82d257
Alex.T <alex.rabota.555@gmail.com>
2021-06-16 08:57:51 +0300

    Changed ask/ask/urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,13 +17,18 @@ from django.contrib import admin
 # from django.urls import path, include
 from django.conf.urls import include, url
 
+from qa import views
+
 urlpatterns = [
-    url(r'^$', include('qa.urls')),
+    # url(r'^$', include('qa.urls')),
+    url(r'^$', view=views.home, name='home'),
     url(r'^login/', include('qa.urls')),
     url(r'^signup/', include('qa.urls')),
-    url(r'^question/', include('qa.urls')),
+    # url(r'^question/', include('qa.urls')),
+    url(r'^question/(?P<id_question>[0-9]+)/$', view=views.question, name='question'),
     url(r'^ask/', include('qa.urls')),
-    url(r'^popular/', include('qa.urls')),
+    # url(r'^popular/', include('qa.urls')),
+    url(r'^popular/', view=views.popular, name='popular'),
     url(r'^new/', include('qa.urls')),
     # path('', include('qa.urls')),
     url(r'^admin/', admin.site.urls),
