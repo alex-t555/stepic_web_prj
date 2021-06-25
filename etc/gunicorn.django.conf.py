22d5f5c043f5c57798f21109a4b8d9e686ddecae
Alex.T <alex.rabota.555@gmail.com>
2021-06-25 03:15:57 +0300

    Changed etc/gunicorn.django.conf.py

--- etc/gunicorn.django.conf.py
+++ etc/gunicorn.django.conf.py
@@ -8,4 +8,7 @@ from pathlib import Path
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 
+# accesslog = str(Path(__file__).absolute().parent.parent / 'access.log')
+# errorlog = str(Path(__file__).absolute().parent.parent / 'error.log')
+
 pythonpath = str(Path(__file__).absolute().parent.parent / 'ask')
